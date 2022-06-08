c001dcce14d3868d56b131a50bb1cd07702c9960
Saulompr99 <saulompr@gmail.com>
2022-06-08 09:03:06 +0100

    Nueva función saludo

--- App.java
+++ App.java
@@ -1,13 +1,23 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mycompany.recuperacion_puk;
 
 /**
  *
  * @author ceti
  */
 public class App {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static boolean saludo() {
+        System.out.println("Hola, estoy en la recuperación de PUK");
+        
+        return true;
+    }
 }
