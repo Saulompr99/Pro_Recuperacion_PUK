bb8f53db22b7533c8649d50bea48de7ebe3fa36d
Saulompr99 <saulompr@gmail.com>
2022-06-08 09:10:43 +0100

    funcion nota añadida

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.recuperacion_puk;
 
@@ -9,5 +9,14 @@ package com.mycompany.recuperacion_puk;
  * @author ceti
  */
 public class App {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int nota() {
+        return 10;
+    }
 }
